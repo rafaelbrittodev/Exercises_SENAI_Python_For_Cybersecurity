68bf7a1eddcb1c52d13b29f88b39d19d26a31487
Rafael Brito <142283293+rafaelbrittodev@users.noreply.github.com>
2026-02-07 10:25:55 -0300

    class-07-02-2026

--- 007.py
+++ 007.py
@@ -2,9 +2,8 @@
 Escreva um programa que peça ao usuário uma letra e imprima se é uma vogal ou consoante.
 '''
 
-letra = input("Digite uma letra: ")
+letra = input("Digite uma letra: ").strip().lower()[0]
 
-letra = letra.lower()
 
 if letra in 'aeiouáéíóúàèìòùâêîôûãõäëïöü':
     print(f"A letra '{letra}' é uma Vogal.")
