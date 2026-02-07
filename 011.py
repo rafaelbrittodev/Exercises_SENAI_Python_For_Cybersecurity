c6f75ca1064197a416b415d53b1b41c2dee98f6a
Rafael Brito <142283293+rafaelbrittodev@users.noreply.github.com>
2026-02-07 10:23:47 -0300

    class-07-02-2026
    Refactor weight reading program to correctly determine the highest and lowest weights.

--- 011.py
+++ 011.py
@@ -2,15 +2,25 @@
 Escreva um programa que leia o peso de 7 pessoas, e no final, mostre qual foi o maior e o menor peso lidos
 '''
 
-def leitura():
+def calc_peso():
     maior_peso = 0
     menor_peso = 0
+
     for i in range(1,8):
-        float(input(f"Digite o peso da pessoa {i}: "))
+        n = float(input(f"Digite o peso da pessoa {i}: "))
+        if maior_peso == 0 and menor_peso == 0:
+            maior_peso = n
+            menor_peso = n
+        elif n > maior_peso:
+            maior_peso = n
+        elif n < menor_peso:
+            menor_peso = n
+        else:
+            print('Error.')
+            break
 
-        if maior_peso < i:
-            maior_peso = i
+    print(f'\nO maior peso é: {maior_peso:.2f}\n'
+          f'O menor peso é: {menor_peso:.2f}')
 
+calc_peso()
 
-
-leitura()
