11aca1668f1f508904bd53df6a008e876268643f
Rafael Brito <142283293+rafaelbrittodev@users.noreply.github.com>
2026-02-07 11:31:45 -0300

    Refactor-07-02-2026

--- 011.py
+++ 011.py
@@ -1,7 +1,7 @@
 '''
 Escreva um programa que leia o peso de 7 pessoas, e no final, mostre qual foi o maior e o menor peso lidos
-'''
 
+#1
 def calc_peso():
     maior_peso = 0
     menor_peso = 0
@@ -21,6 +21,18 @@ def calc_peso():
 
     print(f'\nO maior peso é: {maior_peso:.2f}\n'
           f'O menor peso é: {menor_peso:.2f}')
+
+calc_peso()
+'''
+#2
+def calc_peso():
+    pesos = []
+    for i in range(1,8):
+        n = float(input(f"Digite o peso da pessoa {i}: "))
+        pesos.append(n)
+
+    print(f'\nO maior peso é: {max(pesos)}\n'
+          f'O menor peso é: {min(pesos)}')
 
 calc_peso()
 
